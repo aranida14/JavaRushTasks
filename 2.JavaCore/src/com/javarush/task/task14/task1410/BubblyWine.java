15cd5b8782efb6bae55b56908533c73e468bfffd
dinara <dinara1602@gmail.com>
2017-08-12 23:29:06 +0300

    task1410

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task14.task1410;
+
+/**
+ * Created by Dinara Ibragimova on 12.08.2017.
+ */
+public class BubblyWine extends Wine {
+    @Override
+    public String getHolidayName() {
+        return "Новый Год";
+    }
+}
